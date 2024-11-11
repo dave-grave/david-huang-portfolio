38d0b0d83e643ffcf202557c853a1b94be6821d8
David Huang <djhuang636@gmail.com>
2024-11-10 23:25:02 -0500

    create boilerplate react

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div className="flex items-center
+        justify-center flex-col text-center pt-20
+        pb-12">Intro</div>
+    )
+}
+
+export default Intro;
