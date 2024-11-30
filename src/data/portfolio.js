0fc08d0b143142bde549e80e547f895f75cf2f54
David Huang <djhuang636@gmail.com>
2024-11-29 23:30:55 -0500

    add portfolio projects

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Project 1",
+    imgUrl: "https://via.placeholder.com/150",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://www.google.com",
+  },
+  {
+    title: "David Huang Portfolio",
+    imgUrl: "https://via.placeholder.com/150",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://www.google.com",
+  },
+  {
+    title: "Project 3",
+    imgUrl: "https://via.placeholder.com/150",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://www.google.com",
+  },
+];
