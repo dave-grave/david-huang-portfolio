6ef751e7ef308592fdb6d0fe33a3ef71c3ca01d4
David Huang <djhuang636@gmail.com>
2024-11-30 17:05:26 -0500

    update intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
         <div className="flex items-center
         justify-center flex-col text-center pt-20
         pb-6">
-            <h1 className="text-4xl md:text-7xl 
+            <h1 className="text-4xl md:text-5xl 
             mb-1 md:mb-3 font-bold">David Huang</h1>
             <p className='text-base md:text-xl mb-3
             font-medium'>Computer Engineering @ UofM</p>
