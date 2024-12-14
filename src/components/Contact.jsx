0bf5afed2de6cf56dd6f7e15b83a7d64e4469939
David Huang <djhuang636@gmail.com>
2024-12-14 00:32:09 -0500

    complete contact info and footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,50 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div></div>
+        <div className='flex flex-col mb-10 mx-auto'>
+            <div className='flex justify-center items-center'>
+                <form 
+                    // create custom slug later
+                    // https://getform.io/
+                    action='https://getform.io/f/slug' 
+                    method='POST'
+                    className='flex flex-col w-full md:w-7/12'
+                >
+                    <input 
+                        type='text'
+                        name='name'
+                        placeholder='Name'
+                        className='p-2 bg-transparent border-2 rounded-md
+                        focus:outline-none'
+                    />
+                    <input 
+                        type='text'
+                        name='email'
+                        placeholder='Email'
+                        className='p-2 my-2 bg-transparent border-2 rounded-md
+                        focus:outline-none'
+                    />
+                    <textarea
+                        name="message"
+                        placeholder="Message"
+                        rows="10"
+                        className="p-2 mb-4 bg-transparent border-2
+                        rounded-md focus:outline-none"
+                    />
+                    <button
+                        type="button"
+                        className="text-center inline-block
+                        px-8 py-3 w-max text-base 
+                        font-medium rounded-md text-white
+                        bg-gradient-to-r from-cyan-500 to-yellow-500 
+                        drop-shadow-md hover:stroke-white"
+                    >
+                        Contact me!
+                    </button>
+
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div></div>
+        <div className ="py-5 text-center">
+            {/* code for some icons for socials */}
+            <p className="text-sm mt-2 opacity-50">
+                &copy; {new Date().getFullYear()} 
+                David Huang.
+                All Rights Reserved.
+            </p>
+        </div>
     )
 }
 
