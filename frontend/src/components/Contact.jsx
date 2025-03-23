39da39429317454f6d76ba5eb573e3d9f19762ff
David Huang <djhuang636@gmail.com>
2025-03-22 22:38:01 -0400

    edit contact frontend WIP

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Title";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [response, setResponse] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Submitting...");
+
+    const res = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    setResponse(data.success || data.error);
+  };
+
   return (
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
           // create custom slug later
           // https://getform.io/
-          action="https://getform.io/f/slug"
-          method="POST"
+          onSubmit={handleSubmit}
+          // action="https://getform.io/f/slug"
+          // method="POST"
           className="flex flex-col w-full md:w-7/12"
         >
           <Title>Contact</Title>
@@ -35,7 +61,7 @@ function Contact() {
                         rounded-md focus:outline-none"
           />
           <button
-            type="button"
+            type="submit"
             className="text-center inline-block
                         px-8 py-3 w-max text-base 
                         font-medium rounded-md text-white
@@ -47,6 +73,7 @@ function Contact() {
           </button>
         </form>
       </div>
+      {response && <p className="text-center mt-4">{response}</p>}
     </div>
   );
 }
